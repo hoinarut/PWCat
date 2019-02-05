505bfee7ec1a5ad4fc0f6a304c140de0e2ae41cc
Tudor Hoinaru <thoinaru@192-168-0-136.rdsnet.ro>
2019-02-05 12:56:34 +0200

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 
 import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
 import { ImgCardComponent } from './img-card/img-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     AppRoutingModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
